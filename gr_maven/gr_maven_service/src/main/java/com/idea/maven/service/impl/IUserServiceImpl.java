f5cc6a187fdfcfd6325c11f37644ed2bb31cdffa
XiaoMao1218 <18151621958@126.com>
2017-09-22 10:22:59 +0800

    update

--- gr_maven/gr_maven_service/src/main/java/com/idea/maven/service/impl/IUserServiceImpl.java
+++ gr_maven/gr_maven_service/src/main/java/com/idea/maven/service/impl/IUserServiceImpl.java
@@ -2,22 +2,32 @@ package com.idea.maven.service.impl;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.idea.maven.dao.entity.Account;
-import com.idea.maven.dao.service.user.IUserDao;
+import com.idea.maven.dao.vo.UserVo;
 import com.idea.maven.service.IUserService;
 
 @Service("userService")
 public class IUserServiceImpl implements IUserService{
 	
-	@Autowired
-	private IUserDao userDao;
+//	@Autowired
+//	private IUserDao userDao;
 	
+//	@Override
+//	public List<Account> findUserAll() {
+//		return userDao.findUserAll();
+//	}
+
 	@Override
-	public List<Account> findUserAll() {
-		return userDao.findUserAll();
+	public UserVo findUserByName(String userName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<com.idea.maven.dao.vo.Account> findUserAll() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
